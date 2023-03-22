cff7ebc47100e33d3aab428515969a0eba59d1bb
Geovani Oliveira <108442746+geovanideoliveira@users.noreply.github.com>
2023-03-22 18:47:50 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//php = personal home page
+
+//  variáveis
+//  variavel de variável
+//  concatenar
+//  comparar valores
+//  >= <= ==!=
+//  looping
+//  funções
+//  classes
+
+$nome = 'Geovani';
+
+
+echo 'o meu nome é'.$nome;
+
+
